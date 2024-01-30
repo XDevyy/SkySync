7a9d740cea023edae3ce44c5a2e9ca9e9272d0f2
DevX <112074121+XDevyy@users.noreply.github.com>
2024-01-30 20:05:03 +0100

    Add files via upload

---
+++ use this if you don't trust the exe file.py
@@ -0,0 +1,6 @@
+import subprocess
+
+# Equivalent Python code for the given shell commands
+subprocess.run(["@echo"], shell=True)
+subprocess.run(["title", "ios to desktop"], shell=True)
+subprocess.run(["node", "server.js"], shell=True)
